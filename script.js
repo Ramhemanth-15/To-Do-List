35135561aa8a3afdc3f7e7836e210b317e7e384b
Ramhemanth-15 <144517018+Ramhemanth-15@users.noreply.github.com>
2023-09-09 12:26:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const taskInput = document.getElementById("taskInput");
+    const addTaskButton = document.getElementById("addTask");
+    const taskList = document.getElementById("taskList");
+
+    addTaskButton.addEventListener("click", function () {
+        const taskText = taskInput.value.trim();
+
+        if (taskText !== "") {
+            const li = document.createElement("li");
+            li.innerHTML = `
+                <span>${taskText}</span>
+                <span class="delete">Delete</span>
+            `;
+
+            li.querySelector(".delete").addEventListener("click", function () {
+                li.remove();
+            });
+
+            taskList.appendChild(li);
+            taskInput.value = "";
+        }
+    });
+
+    taskInput.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            addTaskButton.click();
+        }
+    });
+});
